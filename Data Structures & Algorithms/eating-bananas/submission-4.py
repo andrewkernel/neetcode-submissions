815ca8eac2c62fb49e5901e25b57f5b3d908ecd2
Andrew <112019350+andrewkernel@users.noreply.github.com>
2026-07-09 23:46:33 -0500

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def findK(k):
+            total = 0
+
+            for p in piles:
+                total += math.ceil(p/k)
+
+            return total <= h
+
+
+        l, r = 1, max(piles)
+
+        while l < r:
+            k = (l+r) // 2
+
+            if findK(k):
+                r = k
+            else:
+                l = k + 1
+        return l
